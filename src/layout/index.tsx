a9fa39747652f05b0224cd3597878f8a2611f781
Ratana <createbysenpai@gmail.com>
2024-05-02 17:35:06 +0200

    ✨ Feature/sidebar > Create sidebar

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Layout = () => {
   return (
     <div className="w-screen h-screen relative">
       <Navbar />
+      <Sidebar />
       <div className="md:pl-[250px] pl-[60px] pr-[20px] pt-[70px] w-full h-full overflow-y-auto ">
         <Outlet />
       </div>
