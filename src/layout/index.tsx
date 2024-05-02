06b73c3b79ea448ea7f0a27ca5b64542c139eb48
Ratana <createbysenpai@gmail.com>
2024-05-02 16:13:59 +0200

    ✨ Feature/navbar > Create navbar

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <div className="w-screen h-screen relative">
+      <Navbar />
       <div className="md:pl-[250px] pl-[60px] pr-[20px] pt-[70px] w-full h-full overflow-y-auto ">
         <Outlet />
       </div>
