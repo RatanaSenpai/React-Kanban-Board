d9ea5c707a6cb88526225d1d516689be2669aae9
Ratana <createbysenpai@gmail.com>
2024-05-02 15:26:40 +0200

    ✨ Feature/layout > Create layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,8 +2,10 @@ import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="w-screen h-screen relative">
+      <div className="md:pl-[250px] pl-[60px] pr-[20px] pt-[70px] w-full h-full overflow-y-auto ">
+        <Outlet />
+      </div>
     </div>
   );
 };
