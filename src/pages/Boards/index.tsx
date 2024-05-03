e73c8c8915afe0be82bbc8c2e7ca2f02292010e7
Ratana <createbysenpai@gmail.com>
2024-05-03 13:34:38 +0200

    📱 Board > index > Fix responsive

--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -32,7 +32,7 @@ const Home = () => {
       <DragDropContext
         onDragEnd={(result: any) => onDragEnd(result, columns, setColumns)}
       >
-        <div className="w-full flex items-start justify-between px-5 pb-8 md:gap-0 gap-10">
+        <div className="w-full flex items-start justify-between px-5 pb-8  gap-10">
           {Object.entries(columns).map(([columnId, column]: any) => (
             <div className="w-full flex flex-col gap-0" key={columnId}>
               <Droppable droppableId={columnId} key={columnId}>
@@ -64,7 +64,7 @@ const Home = () => {
               </Droppable>
               <div
                 onClick={() => openModal(columnId)}
-                className="flex cursor-pointer items-center justify-center gap-1 py-[10px] md:w-[90%] w-full opacity-90 bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]"
+                className="flex cursor-pointer items-center justify-center gap-1  py-[10px] w-full lg:w-[290px] md:w-[90%] opacity-90 bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]"
               >
                 <AddOutline color={"#555"} />
                 Add Task
